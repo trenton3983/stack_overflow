d15d392041f4fea6971624c2fe540e52a59d8520
Trenton J. McKinney <trenton3983@users.noreply.github.com>
2024-03-14 10:36:09 -0700

    Create SO_Linkonly_Parser.py

---
+++ Tools/SO_Linkonly_Parser.py
@@ -0,0 +1,113 @@
+import requests
+import json
+from datetime import datetime, timedelta
+import configparser
+import time
+import webbrowser
+import logging
+
+# Define constants
+DAYS_IN_MONTH = 32
+BODY_LENGTH_LIMIT = 500
+PAGE_SIZE = 100
+LOG_LEVEL = logging.INFO
+tags = "python;matplotlib"
+start_date = "2012-01-01"
+end_date = "2012-12-31"
+
+# Set up logging
+logging.basicConfig(filename = f"00_log_{start_date}_{end_date}.log", level=LOG_LEVEL)
+
+def month_range(start_date, end_date):
+    """Generate start and end dates for each month in the range."""
+    start_date = start_date.replace(day=1)
+    while start_date < end_date:
+        end_date_month = (start_date + timedelta(days=DAYS_IN_MONTH)).replace(day=1)
+        yield start_date, min(end_date, end_date_month)
+        start_date = end_date_month
+
+# Load the configuration file
+config = configparser.ConfigParser()
+config.read(r'D:\\users\\trenton\\Dropbox\\PythonProjects\\config_api.ini')
+
+# Get the API key from the configuration file
+api_key = config.get('stackapps', 'key')
+
+# Define the API endpoint for questions
+questions_url = "https://api.stackexchange.com/2.3/questions"
+
+# Define the start and end dates
+start_date = datetime.strptime(start_date, "%Y-%m-%d")
+end_date = datetime.strptime(end_date, "%Y-%m-%d")
+
+# Create a list to store the links
+links = []
+
+for start_date_month, end_date_month in month_range(start_date, end_date):
+    # Convert the dates to Unix timestamp
+    start_date_unix = int(start_date_month.timestamp())
+    end_date_unix = int(end_date_month.timestamp())
+
+    # Log start_date_str
+    logging.info(f"Start Date: {start_date_month.strftime('%Y-%m-%d')}, End Date: {end_date_month.strftime('%Y-%m-%d')}")
+
+    page = 1
+    while True:
+        # Define the parameters for the API request
+        params = {
+            "order": "desc",
+            "sort": "creation",
+            "tagged": tags,  # Search for posts with these tags
+            "fromdate": start_date_unix,
+            "todate": end_date_unix,
+            "site": "stackoverflow",
+            "filter": "!-*jbN-o8P3E5",
+            "key": api_key,
+            "page": page,
+            "pagesize": PAGE_SIZE
+        }
+
+        try:
+            # Send the API request
+            response = requests.get(questions_url, params=params)
+            data = json.loads(response.text)
+
+            # If no items are returned, we've reached the end of the results
+            if not data["items"]:
+                break
+
+            # For each question, parse the 'answers' key directly
+            for question in data["items"]:
+                # Check if the question has answers
+                if "answers" in question:
+                    # Filter for answers that may primarily be link-only and exclude links from https://i.stack.imgur.com
+                    link_only_answers = [item for item in question["answers"] if "http" in item["body"]
+                                         and "https://i.stack.imgur.com" not in item["body"]
+                                         and "<code>" not in item["body"]
+                                         and len(item["body"]) < BODY_LENGTH_LIMIT]
+
+                    if link_only_answers:
+                        logging.info(f"Question ID: {question['question_id']}")
+                        # Log the link-only answers
+                        for answer in link_only_answers:
+                            link = f"https://stackoverflow.com/a/{answer['answer_id']}"
+                            logging.info(f"Answer ID: {answer['answer_id']}, Link: {link}")
+                            # Add the link to the list
+                            links.append(link)
+            logging.info(f'Remaining Quota: {data["quota_remaining"]}')
+            if "backoff" in data:
+                time.sleep(data["backoff"])
+
+            # Increment the page number for the next request
+            page += 1
+
+        except requests.exceptions.RequestException as e:
+            logging.error(f"Request failed: {e}")
+            time.sleep(5)
+
+# print the number of links
+print(f"Number of link-only answers: {len(links)}")
+
+# Open the links in a browser
+# for link in links:
+#     webbrowser.open(link)
